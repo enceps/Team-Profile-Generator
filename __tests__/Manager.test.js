f1616661d8883e5b541c94c7685e4abfa3dc082a
enceps <spence.d.cole@gmail.com>
2022-10-25 16:51:54 -0400

    add other classes

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,14 @@
-const Employee = require('./Employee');
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+  constructor(name, id, email, officeNumber) {
+    super(name, id, email);
+    this.officeNumber = officeNumber;
+    this.title = "Manager";
+  }
+
+  getOfficeNumber() {
+    return this.officeNumber;
+  }
+}
+module.exports = Manager;
